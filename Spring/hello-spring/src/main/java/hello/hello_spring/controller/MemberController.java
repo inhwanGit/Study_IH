bfaf8be2e5bb06d126fd5e4741f8950835055d90
InHwan <inhwanpark19991120@gmail.com>
2025-02-02 12:47:08 +0900

    spring component 방벙

--- Spring/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ Spring/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,17 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
 
+    private final MemberService memberService;
+
+    // controller 와 service를 연결
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 }
