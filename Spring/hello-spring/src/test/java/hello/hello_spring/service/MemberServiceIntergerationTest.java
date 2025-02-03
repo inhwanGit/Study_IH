25026d14069320127fcb72327f786c016ce0bc01
InHwan <inhwanpark19991120@gmail.com>
2025-02-03 23:57:48 +0900

    애노테이션 내용  추가

--- Spring/hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntergerationTest.java
+++ Spring/hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntergerationTest.java
@@ -11,8 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional  // Test가 끝나면 롤백을 해준다.
+@SpringBootTest // 스프링 컨테이너와 테스트를 함께 실행한다.
+@Transactional  // 테스트 케이스에 @Transactional 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고, 테스트가 완료 후에 롤백한다.
+                // 이렇게 하면 TEST시 DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지 않는다.
 class MemberServiceIntergerationTest {
 
     @Autowired
