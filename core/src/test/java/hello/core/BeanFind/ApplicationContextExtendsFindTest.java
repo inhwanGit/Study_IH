671df87689d4f6d2359afa9353464ee2d2859d8c
InHwan <inhwanpark19991120@gmail.com>
2025-02-12 18:02:27 +0900

    Test

--- core/src/test/java/hello/core/BeanFind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/BeanFind/ApplicationContextExtendsFindTest.java
@@ -3,6 +3,8 @@ package hello.core.BeanFind;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationContextExtendsFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복오류가 발생한다.")
+    void findBeanByParentTypeDuplicate(){
+        ac.getBean(DiscountPolicy.class);
+    }
 
     @Configuration
     static class TestConfig {
