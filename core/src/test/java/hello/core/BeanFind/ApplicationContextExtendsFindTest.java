1f7170951d78749dd9b29c694f645ecaf1b9e971
InHwan <inhwanpark19991120@gmail.com>
2025-02-13 23:42:38 +0900

    부모 타입으로 조회시, 자식이 둘 이상일떄

--- core/src/test/java/hello/core/BeanFind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/BeanFind/ApplicationContextExtendsFindTest.java
@@ -3,8 +3,10 @@ package hello.core.BeanFind;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +18,15 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복오류가 발생한다.")
     void findBeanByParentTypeDuplicate(){
-        ac.getBean(DiscountPolicy.class);
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
     }
 
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    void findBeanByParentTypeBeanName(){
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
     @Configuration
     static class TestConfig {
         @Bean
