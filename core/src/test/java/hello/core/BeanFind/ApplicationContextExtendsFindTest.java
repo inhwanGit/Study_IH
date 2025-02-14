5632767424acec1d69cb942902b229e6692520d1
InHwan <inhwanpark19991120@gmail.com>
2025-02-14 18:19:32 +0900

    부모 타입으로 모두 조회하기

--- core/src/test/java/hello/core/BeanFind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/BeanFind/ApplicationContextExtendsFindTest.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 public class ApplicationContextExtendsFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
@@ -29,10 +31,20 @@ public class ApplicationContextExtendsFindTest {
     }
 
     @Test
-    @DisplayName("특정 하위 타입으로 조")
+    @DisplayName("특정 하위 타입으로 조회")
     void findBeanBySubType(){
         RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
         org.assertj.core.api.Assertions.assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType(){
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + ", bean = " + beansOfType.get(key));
+        }
     }
 
     @Configuration
