48763cceebe7c8c6df58d5ad7e397fa5ed5cbef3
InHwan <inhwanpark19991120@gmail.com>
2025-02-18 22:35:02 +0900

    MSG

--- core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -32,7 +32,7 @@ class RateDiscountPolicyTest {
         //when
         int discount = rateDiscountPolicy.discount(member,10000);
         //then
-        assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(0);
     }
 
 }
